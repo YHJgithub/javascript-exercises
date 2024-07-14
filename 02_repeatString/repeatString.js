2123d268dead612f397da58fe15d721b985c259e
Harvey Yang <yanghanji1@gmail.com>
2024-07-14 22:31:06 +0800

    02 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let copy = string;
+  if (num === 0) {
+    return "";
+  } else if (num === -1) {
+    return "ERROR";
+  } else if (string === "") {
+    return "";
+  } else {
+    for (let i = 1; i < num; i++) {
+      string += copy;
+    }
+  }
+  return string;
 };
 
 // Do not edit below this line
