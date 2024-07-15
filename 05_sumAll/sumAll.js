912f5fe5ffa15355c1f97a076d6e8454bece1d4d
Harvey Yang <yanghanji1@gmail.com>
2024-07-15 11:52:01 +0800

    参考答案

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  let result = 0;
+  let smallNum = num1 < num2 ? num1 : num2;
+  let bigNum = num1 > num2 ? num1 : num2;
+  for (let i = smallNum; i <= bigNum; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
